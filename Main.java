7b226e8c48d526fe8e3d26d2e056d88e1a33e654
Son1824 <163498771+Son1824@users.noreply.github.com>
2024-10-25 18:45:14 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,18 +3,18 @@ package QueueDemo.ArrayBasedQueue;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Array-Based Queue:");
-        ArrayBasedQueue queue = new ArrayBasedQueue(5);
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
+        ArrayBasedQueue arrayQueue = new ArrayBasedQueue(5);
+        arrayQueue.enqueue(1);
+        arrayQueue.enqueue(2);
+        arrayQueue.enqueue(3);
 
-        queue.displayQueue();
+        arrayQueue.displayQueue();
 
-        System.out.println("Front element (peek): " + queue.peek());
+        System.out.println("Front element (peek): " + arrayQueue.peek());
 
-        System.out.println("Dequeued element: " + queue.dequeue());
-        queue.displayQueue();
+        System.out.println("Dequeued element: " + arrayQueue.dequeue());
+        arrayQueue.displayQueue();
 
-        System.out.println("Is queue empty? " + queue.isEmpty());
+        System.out.println("Is queue empty? " + arrayQueue.isEmpty());
     }
 }
